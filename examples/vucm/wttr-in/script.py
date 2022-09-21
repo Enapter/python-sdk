c8d231a3e3d70d6416db715a357b580cd02cef98
Roman Novatorov <rnovatorov@enapter.com>
2022-09-21 16:45:27 +0300

    examples/vucm/wttr-in: remove redundant device factory class

--- examples/vucm/wttr-in/script.py
+++ examples/vucm/wttr-in/script.py
@@ -1,5 +1,4 @@
 import asyncio
-import contextlib
 import functools
 import os
 
@@ -17,24 +16,6 @@ async def main():
             location=os.environ["WTTR_IN_LOCATION"],
         )
         await enapter.vucm.run(device_factory)
-
-
-class WttrInFactory:
-    def __init__(self, location=None):
-        self.stack = contextlib.AsyncExitStack()
-        if location is None:
-            location = os.environ["WTTR_IN_LOCATION"]
-        self.location = location
-
-    async def __aenter__(self):
-        self.client = await self.stack.enter_async_context(python_weather.Client())
-        return self
-
-    async def __aexit__(self, *exc):
-        await self.stack.aclose()
-
-    def __call__(self, **kwargs):
-        return WttrIn(client=self.client, location=self.location, **kwargs)
 
 
 class WttrIn(enapter.vucm.Device):
