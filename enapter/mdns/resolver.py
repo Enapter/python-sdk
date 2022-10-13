c460b87279ca9931ffe23a268a356965a37f2b8a
Roman Novatorov <rnovatorov@enapter.com>
2022-10-13 20:01:07 +0300

    feat(mdns): add mDNS resolver and bump version to 0.4.0

---
+++ enapter/mdns/resolver.py
@@ -0,0 +1,17 @@
+import dns.asyncresolver
+
+
+class Resolver:
+    def __init__(self, nameservers=("224.0.0.251",), port=5353, timeout=4, lifetime=10):
+        self._resolver = dns.asyncresolver.Resolver()
+        self._resolver.nameservers = list(nameservers)
+        self._resolver.port = port
+        self._resolver.timeout = timeout
+        self._resolver.lifetime = lifetime
+
+    async def resolve(self, host):
+        answer = await self._resolver.resolve(host, "A")
+        if not answer:
+            raise ValueError(f"failed to resolve host address: {host}")
+
+        return answer[0].address
