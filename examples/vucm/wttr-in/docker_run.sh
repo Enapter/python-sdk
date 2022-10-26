61da036542897066f78b5c0e82e85f7bdef45096
Roman Novatorov <rnovatorov@enapter.com>
2022-10-26 11:40:55 +0300

    ci(examples/vucm): disable quiet docker image build

--- examples/vucm/wttr-in/docker_run.sh
+++ examples/vucm/wttr-in/docker_run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_DIR="$(dirname "$0")"
+IMAGE_TAG="${IMAGE_TAG:-"enapter-vucm-examples/$(basename "$SCRIPT_DIR"):latest"}"
+
+docker build --tag "$IMAGE_TAG" "$SCRIPT_DIR"
+
 docker run --rm -it \
+	-e ENAPTER_LOG_LEVEL="${ENAPTER_LOG_LEVEL:-info}" \
 	-e ENAPTER_VUCM_BLOB="$ENAPTER_VUCM_BLOB" \
 	-e WTTR_IN_LOCATION="$WTTR_IN_LOCATION" \
-	"$(docker build --quiet "$(dirname "$0")")"
+	"$IMAGE_TAG"
