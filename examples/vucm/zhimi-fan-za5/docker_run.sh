bdd603aff5556c7ac4719ae92e981f5640112d27
Roman Novatorov <rnovatorov@enapter.com>
2022-10-26 11:59:29 +0300

    examples: vucm: fix exec docker_run from example dir

--- examples/vucm/zhimi-fan-za5/docker_run.sh
+++ examples/vucm/zhimi-fan-za5/docker_run.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-SCRIPT_DIR="$(dirname "$0")"
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 IMAGE_TAG="${IMAGE_TAG:-"enapter-vucm-examples/$(basename "$SCRIPT_DIR"):latest"}"
 
 docker build --tag "$IMAGE_TAG" "$SCRIPT_DIR"
