31b4ae75d61aabab1e4ef825b9f440db992f2911
Roman Novatorov <rnovatorov@enapter.com>
2022-10-26 14:25:34 +0300

    refactor(mdns): configure DNS resolver

--- enapter/mdns/resolver.py
+++ enapter/mdns/resolver.py
@@ -3,7 +3,7 @@ import dns.asyncresolver
 
 class Resolver:
     def __init__(self, nameservers=("224.0.0.251",), port=5353, timeout=4, lifetime=10):
-        self._resolver = dns.asyncresolver.Resolver(configure=False)
+        self._resolver = dns.asyncresolver.Resolver()
         self._resolver.nameservers = list(nameservers)
         self._resolver.port = port
         self._resolver.timeout = timeout
