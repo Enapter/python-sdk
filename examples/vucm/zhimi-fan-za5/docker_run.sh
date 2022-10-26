e0017e80727294441770ecabe2c2fd50993e850f
Roman Novatorov <rnovatorov@enapter.com>
2022-10-26 11:40:55 +0300

    vucm: examples: disable quiet docker image build

--- examples/vucm/zhimi-fan-za5/docker_run.sh
+++ examples/vucm/zhimi-fan-za5/docker_run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_DIR="$(dirname "$0")"
+IMAGE_TAG="${IMAGE_TAG:-"enapter-vucm-examples/$(basename "$SCRIPT_DIR"):latest"}"
+
+docker build --tag "$IMAGE_TAG" "$SCRIPT_DIR"
+
 docker run --rm -it \
+	-e ENAPTER_LOG_LEVEL="${ENAPTER_LOG_LEVEL:-info}" \
 	-e ENAPTER_VUCM_BLOB="$ENAPTER_VUCM_BLOB" \
 	-e MIIO_IP="$MIIO_IP" \
 	-e MIIO_TOKEN="$MIIO_TOKEN" \
-	"$(docker build --quiet "$(dirname "$0")")"
+	"$IMAGE_TAG"
