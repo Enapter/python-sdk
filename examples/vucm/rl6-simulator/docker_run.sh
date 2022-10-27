8c39b8a117fa367a208795ba18402f1abc2f0e66
Roman Novatorov <rnovatorov@enapter.com>
2022-10-27 12:18:39 +0300

    examples: vucm: make it work under both mac and linux

--- examples/vucm/rl6-simulator/docker_run.sh
+++ examples/vucm/rl6-simulator/docker_run.sh
@@ -9,6 +9,7 @@ IMAGE_TAG="${IMAGE_TAG:-"enapter-vucm-examples/$(basename "$SCRIPT_DIR"):latest"
 docker build --tag "$IMAGE_TAG" "$SCRIPT_DIR"
 
 docker run --rm -it \
+	--network host \
 	-e ENAPTER_LOG_LEVEL="${ENAPTER_LOG_LEVEL:-info}" \
 	-e ENAPTER_VUCM_BLOB="$ENAPTER_VUCM_BLOB" \
 	-e ENAPTER_VUCM_CHANNEL_ID="rl6" \
