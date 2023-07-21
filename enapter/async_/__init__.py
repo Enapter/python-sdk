795adec9d8697f7635fa71fbad20e4a6bac42506
Roman Novatorov <rnovatorov@enapter.com>
2023-07-21 12:06:04 +0200

    feat(async): add `run_in_executor` helper

--- enapter/async_/__init__.py
+++ enapter/async_/__init__.py
@@ -1,7 +1,9 @@
 from .generator import generator
 from .routine import Routine
+from .run_in_executor import run_in_executor
 
 __all__ = [
     "generator",
     "Routine",
+    "run_in_executor",
 ]
