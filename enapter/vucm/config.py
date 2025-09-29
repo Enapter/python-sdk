6c11d850dcc022c355ea331f6e1c1cdf559b2500
Roman Novatorov <rnovatorov@enapter.com>
2025-09-29 21:59:03 +0200

    feat: add missing type annotations

--- enapter/vucm/config.py
+++ enapter/vucm/config.py
@@ -1,61 +1,72 @@
 import base64
 import json
 import os
+from typing import MutableMapping, Optional
 
 import enapter
 
 
 class Config:
     @classmethod
-    def from_env(cls, prefix=None, env=os.environ):
+    def from_env(
+        cls, prefix: Optional[str] = None, env: MutableMapping[str, str] = os.environ
+    ) -> "Config":
         if prefix is None:
             prefix = "ENAPTER_VUCM_"
         try:
-            blob = os.environ[prefix + "BLOB"]
+            blob = env[prefix + "BLOB"]
         except KeyError:
             pass
         else:
             config = cls.from_blob(blob)
             try:
-                config.channel_id = os.environ[prefix + "CHANNEL_ID"]
+                config.channel_id = env[prefix + "CHANNEL_ID"]
             except KeyError:
                 pass
             return config
 
-        hardware_id = os.environ[prefix + "HARDWARE_ID"]
-        channel_id = os.environ[prefix + "CHANNEL_ID"]
+        hardware_id = env[prefix + "HARDWARE_ID"]
+        channel_id = env[prefix + "CHANNEL_ID"]
 
-        mqtt_config = enapter.mqtt.Config.from_env(prefix=prefix, env=env)
+        mqtt = enapter.mqtt.Config.from_env(prefix=prefix, env=env)
 
-        start_ucm = os.environ.get(prefix + "START_UCM", "1") != "0"
+        start_ucm = env.get(prefix + "START_UCM", "1") != "0"
 
         return cls(
             hardware_id=hardware_id,
             channel_id=channel_id,
-            mqtt_config=mqtt_config,
+            mqtt=mqtt,
             start_ucm=start_ucm,
         )
 
     @classmethod
-    def from_blob(cls, blob):
+    def from_blob(cls, blob: str) -> "Config":
         payload = json.loads(base64.b64decode(blob))
 
-        mqtt_config = enapter.mqtt.Config(
+        mqtt = enapter.mqtt.Config(
             host=payload["mqtt_host"],
             port=int(payload["mqtt_port"]),
-            tls_ca_cert=payload["mqtt_ca"],
-            tls_cert=payload["mqtt_cert"],
-            tls_secret_key=payload["mqtt_private_key"],
+            tls=enapter.mqtt.TLSConfig(
+                ca_cert=payload["mqtt_ca"],
+                cert=payload["mqtt_cert"],
+                secret_key=payload["mqtt_private_key"],
+            ),
         )
 
         return cls(
             hardware_id=payload["ucm_id"],
             channel_id=payload["channel_id"],
-            mqtt_config=mqtt_config,
+            mqtt=mqtt,
         )
 
-    def __init__(self, hardware_id, channel_id, mqtt_config, start_ucm=True):
+    def __init__(
+        self,
+        hardware_id: str,
+        channel_id: str,
+        mqtt: enapter.mqtt.Config,
+        start_ucm: bool = True,
+    ) -> None:
         self.hardware_id = hardware_id
         self.channel_id = channel_id
-        self.mqtt = mqtt_config
+        self.mqtt = mqtt
         self.start_ucm = start_ucm
