7d34423535b25fe71df45506d9643e45591524b1
Roman Novatorov <rnovatorov@enapter.com>
2025-09-30 10:55:26 +0200

    enapter: mqtt: export `Error`

--- enapter/mqtt/__init__.py
+++ enapter/mqtt/__init__.py
@@ -1,10 +1,12 @@
 from . import api
 from .client import Client
 from .config import Config, TLSConfig
+from .errors import Error
 
 __all__ = [
     "Client",
     "Config",
+    "Error",
     "TLSConfig",
     "api",
 ]
---
+++ enapter/mqtt/errors.py
@@ -0,0 +1,3 @@
+import aiomqtt
+
+Error = aiomqtt.MqttError
