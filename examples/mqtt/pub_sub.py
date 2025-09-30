ec67428c6f841a2dd15bf3141d7dd65c5ec0bdfd
Roman Novatorov <rnovatorov@enapter.com>
2025-09-30 12:33:31 +0200

    examples: mqtt: pub sub: formatting

--- examples/mqtt/pub_sub.py
+++ examples/mqtt/pub_sub.py
@@ -4,6 +4,14 @@ import time
 import enapter
 
 
+async def main() -> None:
+    config = enapter.mqtt.Config(host="127.0.0.1", port=1883)
+    async with enapter.mqtt.Client(config=config) as client:
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(subscriber(client))
+            tg.create_task(publisher(client))
+
+
 async def subscriber(client: enapter.mqtt.Client) -> None:
     async with client.subscribe("/+") as messages:
         async for msg in messages:
@@ -17,14 +25,6 @@ async def publisher(client: enapter.mqtt.Client) -> None:
         except enapter.mqtt.Error as e:
             print("error", e)
         await asyncio.sleep(1)
-
-
-async def main() -> None:
-    config = enapter.mqtt.Config(host="127.0.0.1", port=1883)
-    async with enapter.mqtt.Client(config=config) as client:
-        async with asyncio.TaskGroup() as tg:
-            tg.create_task(subscriber(client))
-            tg.create_task(publisher(client))
 
 
 if __name__ == "__main__":
