712a66def3ec1a1ac21b4a02022fa34d818de7bc
Roman Novatorov <rnovatorov@enapter.com>
2025-10-30 16:48:10 +0100

    fix(examples/standalone): correct psutil-battery example

--- examples/standalone/psutil-battery/script.py
+++ examples/standalone/psutil-battery/script.py
@@ -1,5 +1,4 @@
 import asyncio
-from typing import Any, Dict, Tuple
 
 import psutil
 
@@ -7,39 +6,35 @@ import enapter
 
 
 async def main():
-    await enapter.vucm.run(PSUtilBattery)
+    await enapter.standalone.run(PSUtilBattery())
 
 
-class PSUtilBattery(enapter.vucm.Device):
-    @enapter.vucm.device_task
-    async def data_sender(self):
+class PSUtilBattery(enapter.standalone.Device):
+
+    async def run(self):
         while True:
-            telemetry, properties, delay = await self.gather_data()
-            await self.send_telemetry(telemetry)
+            properties, telemetry = await self.gather_data()
             await self.send_properties(properties)
-            await asyncio.sleep(delay)
+            await self.send_telemetry(telemetry)
+            await asyncio.sleep(10)
 
-    async def gather_data(self) -> Tuple[Dict[str, Any], Dict[str, Any], int]:
+    async def gather_data(self):
         try:
-            battery = psutil.sensors_battery()
+            battery = await asyncio.to_thread(psutil.sensors_battery)
         except Exception as e:
-            await self.log.error(f"failed to gather data: {e}")
-            self.alerts.add("gather_data_error")
-            return {}, {}, 10
-        self.alerts.clear()
+            await self.logger.error(f"failed to gather data: {e}")
+            return {}, {"alerts": ["gather_data_error"]}
 
-        telemetry: Dict[str, Any] = {}
-        properties: Dict[str, Any] = {"battery_installed": battery is not None}
+        if battery is None:
+            return {"battery_installed": False}, {}
 
-        if battery is not None:
-            telemetry = {
-                "charge_percent": battery.percent,
-                "power_plugged": battery.power_plugged,
-            }
-            if not battery.power_plugged:
-                telemetry["time_until_full_discharge"] = battery.secsleft
-
-        return telemetry, properties, 5
+        return {"battery_installed": True}, {
+            "charge_percent": battery.percent,
+            "power_plugged": battery.power_plugged,
+            "time_until_full_discharge": (
+                battery.secsleft if not battery.power_plugged else None
+            ),
+        }
 
 
 if __name__ == "__main__":
