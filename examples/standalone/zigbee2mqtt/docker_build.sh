8102cc223b3b725c32a15f1cb9822312f02ed5c6
Roman Novatorov <rnovatorov@enapter.com>
2025-11-03 15:07:16 +0100

    fix(examples/standalone): attempt fix for zigbee2mqtt

--- examples/standalone/zigbee2mqtt/docker_build.sh
+++ examples/standalone/zigbee2mqtt/docker_build.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 IFS=$'\n\t'
 
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
-IMAGE_TAG="${IMAGE_TAG:-"enapter-vucm-examples/$(basename "$SCRIPT_DIR"):latest"}"
+IMAGE_TAG="${IMAGE_TAG:-"enapter-standalone-examples/$(basename "$SCRIPT_DIR"):latest"}"
 
 docker build --progress=plain --tag "$IMAGE_TAG" "$SCRIPT_DIR"
