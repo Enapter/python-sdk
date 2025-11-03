258b2b31867d96c389079513007e572684dc0da0
Roman Novatorov <rnovatorov@enapter.com>
2025-11-03 09:40:58 +0100

    feat(standalone/vucm): use different intervals for properties and telemetry

--- src/enapter/standalone/ucm.py
+++ src/enapter/standalone/ucm.py
@@ -7,10 +7,19 @@ from .device_protocol import CommandResult
 class UCM(Device):
 
     async def run(self) -> None:
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(self.properties_sender())
+            tg.create_task(self.telemetry_sender())
+
+    async def properties_sender(self) -> None:
         while True:
             await self.send_properties({"virtual": True, "lua_api_ver": 1})
-            await self.send_telemetry({})
             await asyncio.sleep(30)
+
+    async def telemetry_sender(self) -> None:
+        while True:
+            await self.send_telemetry({})
+            await asyncio.sleep(1)
 
     async def cmd_reboot(self, *args, **kwargs) -> CommandResult:
         raise NotImplementedError
