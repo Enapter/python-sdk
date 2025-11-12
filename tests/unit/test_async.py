024a1d71fcc37888c62b01a828a68e56d8fca394
Roman Novatorov <rnovatorov@enapter.com>
2025-11-12 15:49:50 +0100

    add units tests for `async_.Routine`

--- tests/unit/test_async.py
+++ tests/unit/test_async.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pytest
 
 import enapter
@@ -10,7 +12,7 @@ class TestGenerator:
         async def agen():
             yield 1
             yield 2
-            yield 3
+            yield 3  # pragma: no cover
 
         async with agen() as g:
             assert await g.__anext__() == 1
@@ -18,3 +20,70 @@ class TestGenerator:
 
         with pytest.raises(StopAsyncIteration):
             await g.__anext__()
+
+
+class TestRoutine:
+
+    async def test_run_not_implemented(self):
+        class R(enapter.async_.Routine):
+            pass
+
+        with pytest.raises(TypeError):
+            R()
+
+    async def test_start_twice(self) -> None:
+        started = asyncio.Event()
+
+        class R(enapter.async_.Routine):
+            async def _run(self):
+                started.set()
+                await asyncio.Event().wait()
+
+        async with R() as r:
+            await started.wait()
+            with pytest.raises(RuntimeError):
+                await r.start()
+
+    async def test_stop_without_start(self) -> None:
+        class R(enapter.async_.Routine):
+            async def _run(self):  # pragma: no cover
+                await asyncio.Event().wait()
+
+        r = R()
+        with pytest.raises(RuntimeError):
+            await r.stop()
+
+    async def test_context_manager(self):
+        started = asyncio.Event()
+        done = asyncio.Event()
+
+        class R(enapter.async_.Routine):
+            async def _run(self):
+                started.set()
+                try:
+                    await asyncio.Event().wait()
+                finally:
+                    done.set()
+
+        async with R():
+            await started.wait()
+
+        assert done.is_set()
+
+    async def test_task_group(self):
+        started = asyncio.Event()
+        done = asyncio.Event()
+
+        class R(enapter.async_.Routine):
+            async def _run(self):
+                started.set()
+                try:
+                    await asyncio.Event().wait()
+                finally:
+                    done.set()
+
+        async with asyncio.TaskGroup() as tg:
+            async with R(task_group=tg):
+                await started.wait()
+
+        assert done.is_set()
