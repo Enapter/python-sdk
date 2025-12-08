2063c48ea1065d000e2bb644b573c774f312c58f
Roman Novatorov <rnovatorov@enapter.com>
2025-12-08 18:16:10 +0100

    http: api: devices: generate device name if not provided

--- src/enapter/cli/http/api/device_create_standalone_command.py
+++ src/enapter/cli/http/api/device_create_standalone_command.py
@@ -11,15 +11,16 @@ class DeviceCreateStandaloneCommand(cli.Command):
         parser = parent.add_parser(
             "create-standalone", formatter_class=argparse.ArgumentDefaultsHelpFormatter
         )
-        parser.add_argument("name", help="Name of the standalone device to create")
         parser.add_argument(
-            "-s", "--site-id", help="Site ID to create device in", default=None
+            "-n", "--name", help="Name of the standalone device to create"
         )
+        parser.add_argument("--slug", help="Slug of the standalone device to create")
+        parser.add_argument("-s", "--site-id", help="Site ID to create device in")
 
     @staticmethod
     async def run(args: argparse.Namespace) -> None:
         async with http.api.Client(http.api.Config.from_env()) as client:
             device = await client.devices.create_standalone(
-                args.name, site_id=args.site_id
+                name=args.name, site_id=args.site_id, slug=args.slug
             )
             print(json.dumps(device.to_dto()))
