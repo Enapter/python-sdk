13f563058670d94a1018948a2e426854f4efcd3e
Roman Novatorov <rnovatorov@enapter.com>
2026-03-02 14:55:30 +0100

    fix(http-api): use `engine` key for rule engine response parsing

--- src/enapter/http/api/rule_engine/client.py
+++ src/enapter/http/api/rule_engine/client.py
@@ -14,19 +14,19 @@ class Client:
         url = self._url(site_id)
         response = await self._client.get(url)
         await api.check_error(response)
-        return Engine.from_dto(response.json()["rule_engine"])
+        return Engine.from_dto(response.json()["engine"])
 
     async def suspend(self, site_id: str | None = None) -> Engine:
         url = f"{self._url(site_id)}/suspend"
         response = await self._client.post(url)
         await api.check_error(response)
-        return Engine.from_dto(response.json()["rule_engine"])
+        return Engine.from_dto(response.json()["engine"])
 
     async def resume(self, site_id: str | None = None) -> Engine:
         url = f"{self._url(site_id)}/resume"
         response = await self._client.post(url)
         await api.check_error(response)
-        return Engine.from_dto(response.json()["rule_engine"])
+        return Engine.from_dto(response.json()["engine"])
 
     def _url(self, site_id: str | None) -> str:
         if site_id is not None:
